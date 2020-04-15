932fd29eb288d23aa8a63290e5fba152ad755271
mawinkler <winkler.info@icloud.com>
2020-04-15 08:43:22 +0200

    fix in detection of aws or gcp config

--- migrate.sh
+++ migrate.sh
@@ -10,11 +10,11 @@ echo "Migrating from ${LOCAL} to ${SERVER}/workdir"
 cp ~/.ssh/id_rsa.pub ${SERVER}/workdir/.ssh/id_rsa.pub
 cp ~/.ssh/id_rsa ${SERVER}/workdir/.ssh/id_rsa
 
-if test -f "${AWS}"; then
+if test -d "${AWS}"; then
   cp -r ${AWS} ${SERVER}/workdir/
   cp ~/.ssh/moadsd-ng ${SERVER}/workdir/.ssh/moadsd-ng
 fi
-if test -f "${GCP}"; then
+if test -d "${GCP}"; then
   cp -r ${GCP} ${SERVER}/workdir/
   cp ~/ansible.json ${SERVER}/workdir/
 fi
