24f891fed0bb4415a46cba0fb5daf8adaf66de93
mawinkler <winkler.info@icloud.com>
2020-04-17 10:29:49 +0200

    streamlined build process

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-echo "Building MOADSD-NG-SERVER Container Image"
-docker-compose build
+echo "Generating Environment File"
+echo "UID=$(id -u)" > .env
+echo "GID=$(id -g)" >> .env
 
-echo $'\nStarting MOADSD-NG-SERVER Container'
+#echo "Building MOADSD-NG-SERVER Container Image"
+docker-compose build moadsd-ng-server
+
 IMAGE=$(docker images --format "{{.Repository}}" | grep moadsd-ng-server)
+echo "Starting MOADSD-NG-SERVER Container from image ${IMAGE}"
 docker run -d --rm --name=moadsd-ng-server ${IMAGE} -c "/bin/sleep 60"
 
-echo $'\nFetch Home Directory from Container'
+echo "Fetch Home Directory from Container"
 CONTAINER=$(docker ps --format "{{.Names}}" | grep moadsd-ng-server)
 docker cp ${CONTAINER}:/tmp/ansible.tgz .
 
-echo $'\nStopping MOADSD-NG-SERVER Container'
+echo "Stopping MOADSD-NG-SERVER Container"
 docker stop ${CONTAINER}
 
-echo $'\nPopulating workdir'
+echo "Populating workdir"
 mkdir -p workdir
 tar xpzf ansible.tgz --strip-components=2 -C ./workdir
 rm ansible.tgz
